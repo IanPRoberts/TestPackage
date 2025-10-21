31b52ff957def9cdde64c9ff6479845ab4b1cd60
Ian Roberts <ian.roberts@ndm.ox.ac.uk>
2025-10-21 11:44:19 +0100

    Test functions and documentation etc

---
+++ R/f.R
@@ -0,0 +1,11 @@
+#' Test function
+#' 
+#' @param x Test input
+#'
+#' @return Not much
+#' 
+#' @export
+f <-
+function( x ){
+  print( x^2 )
+}
---
+++ tests/testthat/test-f.R
@@ -0,0 +1,3 @@
+test_that("test_test", {
+  expect_equal( f( 2 ), 4)
+})
